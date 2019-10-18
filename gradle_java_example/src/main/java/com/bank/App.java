ae69386fd75e8b0fd26dd8c080e62f96c984d4ec
GBS05074 <vignesh.r@sella.it>
2019-10-18 16:12:49 +0530

    sample push test

--- gradle_java_example/src/main/java/com/bank/App.java
+++ gradle_java_example/src/main/java/com/bank/App.java
@@ -10,6 +10,6 @@ import greet.hello;
 public class App{
    public static void main( String[] args ){
    	hello helloer=new hello();
-      helloer.sayHello("Vignesh");
+      helloer.sayHello("VigneshR");
    }
 }

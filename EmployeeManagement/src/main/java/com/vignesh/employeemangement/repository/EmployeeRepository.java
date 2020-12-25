f6765e45437809c957e59bb39f6f538ffddbd4f2
vigneshr6 <iamvigneshr6@gmail.com>
2020-12-25 14:30:14 +0530

    rollback

--- EmployeeManagement/src/main/java/com/vignesh/employeemangement/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.vignesh.employeemangement.repository;
-
-import com.vignesh.employeemangement.model.Employee;
-import org.springframework.data.repository.CrudRepository;
-
-public interface EmployeeRepository extends CrudRepository<Employee,Long> {
-}
